60e6dd3df7a68d22a88e8654c1f5143315e8909e
yongjun-shin <syj1634@naver.com>
2023-09-18 04:49:29 +0900

    Feat: create todo 기능 구현 완료

--- src/pages/todo.js
+++ src/pages/todo.js
@@ -1,8 +1,51 @@
+import Axios from "axios";
+import { useEffect, useState } from "react";
+
 export function Todo() {
+  const [todos, setTodos] = useState();
+  const jwt = localStorage.getItem('jwt');
+  const [create, setCreate] = useState('');
+
+  const get_delete_headers = {'Authorization': `Bearer ${jwt}`};
+  const create_update_headers = {
+    'Authorization': `Bearer ${jwt}`,
+    'Content-Type': 'application/json'
+  }
+
+  const getTodos = () => {
+    Axios.get('https://www.pre-onboarding-selection-task.shop/todos/', {headers: get_delete_headers})
+    .then((response)=>{
+      console.log(response.data);
+      setTodos(response.data);
+    })
+    .catch((error)=>{
+      console.error(error);
+    });
+  };
+  
+  const createTodo = () => {
+    Axios.post('https://www.pre-onboarding-selection-task.shop/todos/', {todo: create}, {headers: create_update_headers})
+      .then((response)=>{
+        console.log(response.data);
+      })
+      .catch((error)=>{
+        console.error(error);
+      });
+  };
+
+  const handleInputChange = (e) => {
+    setCreate(e.target.value);
+  };
+
+  useEffect(()=>{
+    getTodos();
+  }, []);
 
   return (
     <>
-    <p>안녕</p>
+    <input data-testid="new-todo-input" onChange={handleInputChange}/>
+    <button data-testid="new-todo-add-button" onClick={createTodo}>추가</button>
+    
     </>
   );
 }

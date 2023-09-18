1d90003b1275befb19c5fb9b9f8bbc9b5756ce58
yongjun-shin <syj1634@naver.com>
2023-09-19 01:21:30 +0900

    Feat: update 기능까지 구현 완료. But, 원치않는 동작 1개 존재

--- src/pages/todo.js
+++ src/pages/todo.js
@@ -4,39 +4,60 @@ import { useEffect, useState } from "react";
 export function Todo() {
   const jwt = localStorage.getItem('jwt');
   const [create, setCreate] = useState('');
-  const [todolist, setTodoList] = useState(<></>);
+  const [todolist, setTodoList] = useState([]);
+  const [modifyId, setModifyId] = useState(null);
+  const [modifyStr, setModifyStr] = useState('');
+  const [isModifyMode, setIsModifyMode] = useState(false);
 
   const get_delete_headers = {'Authorization': `Bearer ${jwt}`};
   const create_update_headers = {
     'Authorization': `Bearer ${jwt}`,
     'Content-Type': 'application/json'
-  }
+  };
 
   const getTodos = () => {
     Axios.get('https://www.pre-onboarding-selection-task.shop/todos/', {headers: get_delete_headers})
     .then((response)=>{
       console.log(response.data);
-      const todoItems = response.data.map((item, index) => (
-        <li key={index}>
-          <label>
-            <input type="checkbox" />
-            <span>{item.todo}</span>
-          </label>
-          <button data-testid="modify-button" >수정</button>
-          <button data-testid="delete-button" onClick={() => deleteTodo(item.id)}>삭제</button>
-        </li>
-      ));
-      setTodoList(todoItems);
+      setTodoList(response.data);
     })
     .catch((error)=>{
       console.error(error);
     });
   };
-  
+
   const createTodo = () => {
     Axios.post('https://www.pre-onboarding-selection-task.shop/todos/', {todo: create}, {headers: create_update_headers})
       .then((response)=>{
         console.log(response.data);
+        setCreate('');
+        getTodos();
+      })
+      .catch((error)=>{
+        console.error(error);
+      });
+  };
+
+  const activateUpdate = (id, str) => {
+    setModifyId(id);
+    setModifyStr(str);
+    setIsModifyMode(true);
+  };
+
+  const cancelUpdate = () => {
+    setModifyId(null);
+    setModifyStr('');
+    setIsModifyMode(false);
+  };
+
+  const updateTodo = (id, str, iscom) => {
+    console.log('hi');
+    Axios.put(`https://www.pre-onboarding-selection-task.shop/todos/${id}`, {todo: str, isCompleted: iscom}, {headers: create_update_headers})
+      .then((response)=>{
+        console.log(response.data);
+        setModifyId(null);
+        setModifyStr('');
+        setIsModifyMode(false);
         getTodos();
       })
       .catch((error)=>{
@@ -65,10 +86,31 @@ export function Todo() {
 
   return (
     <>
-    <input data-testid="new-todo-input" onChange={handleInputChange}/>
+    <input data-testid="new-todo-input" onChange={handleInputChange} value={create}/>
     <button data-testid="new-todo-add-button" onClick={createTodo}>추가</button>
     <div>
-      {todolist}
+      {todolist.map((item) => (
+        <li key={item.id}>
+          <label>
+            <input type="checkbox" checked={item.isCompleted} onChange={() => updateTodo(item.id, item.todo, !item.isCompleted)} />
+            {modifyId === item.id && isModifyMode ? (
+              <>
+                <input data-testid="modify-input" value={modifyStr} onChange={(e) => setModifyStr(e.target.value)} />
+                <button data-testid="submit-button" onClick={() => updateTodo(item.id, modifyStr, item.isCompleted)}>제출</button>
+                <button data-testid="cancel-button" onClick={cancelUpdate}>취소</button>
+              </>
+            ) : (
+              <span>{item.todo}</span>
+            )}
+          </label>
+          {!isModifyMode && (
+            <>
+              <button data-testid="modify-button" onClick={() => activateUpdate(item.id, item.todo)}>수정</button>
+              <button data-testid="delete-button" onClick={() => deleteTodo(item.id)}>삭제</button>
+            </>
+          )}
+        </li>
+      ))}
     </div>
     </>
   );
